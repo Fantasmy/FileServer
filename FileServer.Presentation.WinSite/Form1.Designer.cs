71362affb13a7badd7ddaaa92d9dc740ce9b24aa
Jenny <fantazmit@gmail.com>
2018-06-30 18:20:29 +0200

    Finished visual form design. Close #1, close #3, close #8

--- FileServer.Presentation.WinSite/Form1.Designer.cs
+++ FileServer.Presentation.WinSite/Form1.Designer.cs
@@ -28,13 +28,241 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.nameBox = new System.Windows.Forms.TextBox();
+            this.surnameBox = new System.Windows.Forms.TextBox();
+            this.dniBox = new System.Windows.Forms.TextBox();
+            this.idBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.configuraciónToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.appconfigToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.variableDeEntornoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.guardarEnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.jSONToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.xMLToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.txtToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.menuStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Impact", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(235, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Formulario Estudiantes";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(128, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nombre:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(128, 215);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(155, 259);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "DNI:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(166, 300);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(30, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Id:";
+            // 
+            // nameBox
+            // 
+            this.nameBox.Location = new System.Drawing.Point(219, 173);
+            this.nameBox.Name = "nameBox";
+            this.nameBox.Size = new System.Drawing.Size(247, 26);
+            this.nameBox.TabIndex = 5;
+            // 
+            // surnameBox
+            // 
+            this.surnameBox.Location = new System.Drawing.Point(219, 212);
+            this.surnameBox.Name = "surnameBox";
+            this.surnameBox.Size = new System.Drawing.Size(247, 26);
+            this.surnameBox.TabIndex = 6;
+            // 
+            // dniBox
+            // 
+            this.dniBox.Location = new System.Drawing.Point(219, 256);
+            this.dniBox.Name = "dniBox";
+            this.dniBox.Size = new System.Drawing.Size(247, 26);
+            this.dniBox.TabIndex = 7;
+            // 
+            // idBox
+            // 
+            this.idBox.Location = new System.Drawing.Point(219, 297);
+            this.idBox.Name = "idBox";
+            this.idBox.Size = new System.Drawing.Size(247, 26);
+            this.idBox.TabIndex = 8;
+            // 
+            // addButton
+            // 
+            this.addButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.addButton.Location = new System.Drawing.Point(570, 278);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(75, 31);
+            this.addButton.TabIndex = 9;
+            this.addButton.Text = "Añadir";
+            this.addButton.UseVisualStyleBackColor = true;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.configuraciónToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 38);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // configuraciónToolStripMenuItem
+            // 
+            this.configuraciónToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.appconfigToolStripMenuItem,
+            this.variableDeEntornoToolStripMenuItem});
+            this.configuraciónToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI Black", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.configuraciónToolStripMenuItem.Name = "configuraciónToolStripMenuItem";
+            this.configuraciónToolStripMenuItem.Size = new System.Drawing.Size(218, 34);
+            this.configuraciónToolStripMenuItem.Text = "Configuración en:";
+            // 
+            // appconfigToolStripMenuItem
+            // 
+            this.appconfigToolStripMenuItem.Name = "appconfigToolStripMenuItem";
+            this.appconfigToolStripMenuItem.Size = new System.Drawing.Size(312, 34);
+            this.appconfigToolStripMenuItem.Text = "App.config";
+            // 
+            // variableDeEntornoToolStripMenuItem
+            // 
+            this.variableDeEntornoToolStripMenuItem.Name = "variableDeEntornoToolStripMenuItem";
+            this.variableDeEntornoToolStripMenuItem.Size = new System.Drawing.Size(312, 34);
+            this.variableDeEntornoToolStripMenuItem.Text = "Variable de entorno";
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Dock = System.Windows.Forms.DockStyle.None;
+            this.menuStrip2.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.guardarEnToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(545, 204);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(132, 33);
+            this.menuStrip2.TabIndex = 11;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // guardarEnToolStripMenuItem
+            // 
+            this.guardarEnToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.jSONToolStripMenuItem,
+            this.xMLToolStripMenuItem,
+            this.txtToolStripMenuItem});
+            this.guardarEnToolStripMenuItem.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guardarEnToolStripMenuItem.Name = "guardarEnToolStripMenuItem";
+            this.guardarEnToolStripMenuItem.Size = new System.Drawing.Size(124, 29);
+            this.guardarEnToolStripMenuItem.Text = "Guardar en:";
+            // 
+            // jSONToolStripMenuItem
+            // 
+            this.jSONToolStripMenuItem.Name = "jSONToolStripMenuItem";
+            this.jSONToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.jSONToolStripMenuItem.Text = "JSON";
+            // 
+            // xMLToolStripMenuItem
+            // 
+            this.xMLToolStripMenuItem.Name = "xMLToolStripMenuItem";
+            this.xMLToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.xMLToolStripMenuItem.Text = "XML";
+            // 
+            // txtToolStripMenuItem
+            // 
+            this.txtToolStripMenuItem.Name = "txtToolStripMenuItem";
+            this.txtToolStripMenuItem.Size = new System.Drawing.Size(252, 30);
+            this.txtToolStripMenuItem.Text = "txt";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.idBox);
+            this.Controls.Add(this.dniBox);
+            this.Controls.Add(this.surnameBox);
+            this.Controls.Add(this.nameBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Controls.Add(this.menuStrip2);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox nameBox;
+        private System.Windows.Forms.TextBox surnameBox;
+        private System.Windows.Forms.TextBox dniBox;
+        private System.Windows.Forms.TextBox idBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem configuraciónToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem appconfigToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem variableDeEntornoToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem guardarEnToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem jSONToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem xMLToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem txtToolStripMenuItem;
     }
 }
 
